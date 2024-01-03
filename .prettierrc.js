c6cbaa263230237eb145cefaab04a80e388cec8c
Liren Tu <tuliren@gmail.com>
2024-01-03 09:57:26 -0800

    Update repo config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ...require('eslint-config-mantine/.prettierrc.js'),
+  trailingComma: 'es5',
   singleQuote: true,
   tabWidth: 2,
+  importOrder: ['^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
--- tests/helper.test.ts
+++ tests/helper.test.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@jest/globals';
+
 import { add } from '../src/helper';
 
 test('add', () => {
