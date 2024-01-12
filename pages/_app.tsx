77afef3828fffc2bd764de6d19d7f89e5425743d
Liren Tu <tuliren@gmail.com>
2024-01-12 10:53:41 -0800

    Update dependencies and yarn lock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,8 @@ import { Notifications } from '@mantine/notifications';
 import PlausibleProvider from 'next-plausible';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { FC } from 'react';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => {
+export default function MyApp({ Component, pageProps }: AppProps) {
   // Uses NEXT_PUBLIC_VERCEL_ENV instead of NODE_ENV so we can exclude previews from analytics collection.
   // see https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
   const enableAnalytics = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
@@ -38,6 +37,4 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       </PlausibleProvider>
     </>
   );
-};
-
-export default App;
+}
