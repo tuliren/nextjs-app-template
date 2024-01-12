ddfc40dcce781705669e229340c1967aa4c4035b
Liren Tu <tuliren@gmail.com>
2024-01-12 11:01:48 -0800

    Use yarn 4 and upgrade nextjs (#84)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import { AppShell, Container, MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import PlausibleProvider from 'next-plausible';
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
-
+export default function MyApp({ Component, pageProps }: AppProps) {
   // Uses NEXT_PUBLIC_VERCEL_ENV instead of NODE_ENV so we can exclude previews from analytics collection.
   // see https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
   const enableAnalytics = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
