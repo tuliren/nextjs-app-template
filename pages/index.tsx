9ef08e25cc300cc2651ec4f81eaf7e6a702df67c
Liren Tu <tuliren@gmail.com>
2024-08-23 21:02:19 -0700

    Add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import { Container } from '@mantine/core';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <Container size="xs">coming soon...</Container>;
+  return (
+    <Container size="xs">
+      <h1 className="text-3xl">Hello world!</h1>
+    </Container>
+  );
 };
 
 export default Home;
